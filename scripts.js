fa032d49ef9be4f9095ca33183985bc34af5bbab
RenanSilvaMartins <renansilva.m09@gmail.com>
2024-03-15 07:36:56 -0300

    Adicionei a variável nome

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
 
 function alertar(event){
     if(true){
-        alert("Voce clicou no botão!!!" + event);
+        alert("Voce clicou no botão!!!" + " " + nome.value);
     }
 }
